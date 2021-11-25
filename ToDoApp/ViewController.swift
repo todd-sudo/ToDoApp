b3d87d3a27cc50317ee5eed62e4991e2ac06cb08
Egor <egoreast.true@gmail.com>
2021-11-25 16:41:08 +0300

    не подгружается база FAIL))

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -1,23 +1,69 @@
 import UIKit
 
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Получение контекста
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var models = [ToDoListItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "ToDo List"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter new item", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "Submit", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            
+            self?.createItem(name: text)
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    
+    // MARK: CRUD data
     
     // Получение списка объектов
     func getAllItems() {
         do {
-            let items = try context.fetch(ToDoListItem.fetchRequest())
-            
+            models = try context.fetch(ToDoListItem.fetchRequest())
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
         catch {
-            // error
+            print("error")
         }
     }
     
@@ -29,6 +75,7 @@ class ViewController: UIViewController {
         
         do {
             try context.save()
+            getAllItems()
         }
         catch {
             
