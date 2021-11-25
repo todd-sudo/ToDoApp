b47ebe4a9b293a6e47482598c66bd2a5e4723d8b
Egor <egoreast.true@gmail.com>
2021-11-25 15:08:25 +0300

    init project

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by JoyDev on 25.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
